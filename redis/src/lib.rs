40d41fee3e238021314ccb0d739993a011301b80
David Liu <david-khala@hotmail.com>
2024-07-17 13:18:04 +0800

    add redis-rs sample

--- redis/src/lib.rs
+++ redis/src/lib.rs
@@ -16,11 +16,27 @@ pub fn create_client(host: &str, password: Option<&str>, port: Option<i32>) -> C
 mod tests {
     use super::*;
 
-
-    #[test]
-    fn connect_redis_cloud() {
+    fn connect_redis_cloud() -> RedisResult<Connection> {
         let endpoint = "redis-11516.c295.ap-southeast-1-1.ec2.redns.redis-cloud.com";
         let mut client = create_client(endpoint, Some("jiQ2LPgrn9ZIKQjmqm1o6DbF1bSHKO5w"), Some(11516));
         assert!(client.check_connection());
+        return client.get_connection()
+    }
+    fn fetch_an_integer(key:&str, value:isize) -> redis::RedisResult<isize> {
+        // connect to redis
+
+        let mut con = connect_redis_cloud()?;
+        // throw away the result, just make sure it does not fail
+        let _: () = con.set(key, value)?;
+        // read back the key and return it.
+        //  Because the return value from the function is a result for integer this will automatically convert into one.
+        con.get(key)
+    }
+
+    #[test]
+    fn redis_rs_example() {
+        let r = fetch_an_integer("my_key", 42);
+        assert_eq!(42,r.unwrap());
+
     }
 }
