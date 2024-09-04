ea602f3ab95f697b8ed7ec335424fe8ebfbbc28a
David Chan <davidchan@memken.com>
2024-09-04 23:58:08 +0800

    update comment on example grpc sample

--- example/src/main.rs
+++ example/src/main.rs
@@ -7,7 +7,8 @@ mod http;
 #[tokio::main]
 async fn main() -> http::HttpResult<()>{
     // TODO!
-    // grpc more throughly example
+    // 1. add new proto file and compile with protoc or using build.rs
+    // 2. hook up with the serviec(1) and start using gRpc to communicate among services 
     let addr = std::env::var("HTTP_ADDR").unwrap_or("[::1]:50055".to_string());
     let _ = novax_grpc::grpc_svc(addr, async move{
         // todo
